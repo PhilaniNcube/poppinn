32412fead17c20cbe6ea73235bf003d613126082
Philani Ncube <ncbphi001@gmail.com>
2021-08-14 20:30:15 +0200

    remove box shadow

--- components/Hompage/FeaturedProperty.js
+++ components/Hompage/FeaturedProperty.js
@@ -32,8 +32,6 @@ const Wrapper = styled.section`
       height: 100%;
       padding: 5rem;
       /* background-color: var(--light); */
-      box-shadow: rgba(0, 0, 0, 0.1) 0px 20px 25px -5px,
-        rgba(0, 0, 0, 0.04) 0px 10px 10px -5px;
 
       h3 {
         font-size: var(--medium-title);
