de9db764abe66e32e2313178609c0cf5b710bd2b
Philani Ncube <ncbphi001@gmail.com>
2021-08-16 17:58:57 +0200

    test

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,9 @@
 import { Fragment } from 'react';
-import styled from 'styled-components';
 import ContactForm from '../components/Hompage/ContactForm';
 import Description from '../components/Hompage/Description';
 import FeaturedProperty from '../components/Hompage/FeaturedProperty';
 import Hero from '../components/Hompage/Hero';
 import SectionOne from '../components/Hompage/SectionOne';
-import SectionThree from '../components/Hompage/SectionThree';
 import SectionTwo from '../components/Hompage/SectionTwo';
 
 export default function Home() {
