63ff486c5ba9e6e0b3d5da41ab1a389bc3fc7d38
Philani Ncube <ncbphi001@gmail.com>
2021-08-16 17:33:48 +0200

    roboto

--- pages/_document.js
+++ pages/_document.js
@@ -20,16 +20,9 @@ export default class MyDocument extends Document {
         styles: (
           <>
             <Head>
-              <link rel="preconnect" href="https://fonts.googleapis.com" />
-              <link
-                rel="preconnect"
-                href="https://fonts.gstatic.com"
-                crossorigin
-              />
-              <link
-                href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600;700&display=swap"
-                rel="stylesheet"
-              />
+                <link rel="preconnect" href="https://fonts.googleapis.com"/>
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+                <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700;900&display=swap" rel="stylesheet"/>
             </Head>
             {initialProps.styles}
             {sheet.getStyleElement()}
